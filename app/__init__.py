481c718c582022438841003a91db3caf201c3e6c
KalyanJyothula <38788245+kalyanjyothula@users.noreply.github.com>
2025-11-10 19:30:18 +0530

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -9,7 +9,13 @@ logging.getLogger("httpx").setLevel(logging.WARNING)
 
 def create_app(config_object: type[Config] | None = None):
     app = Flask(__name__)
-    CORS(app, resources={r"/*": {"origins": ["http://localhost:3000"]}})
+    CORS(app,
+         resources={r"/*": {"origins": [
+            "http://localhost:3000",
+            "http://127.0.0.1:3000",
+            "https://kalyanjyothula.fun"
+         ]}}
+    )
 
 
     app.config.from_object(config_object or Config)
